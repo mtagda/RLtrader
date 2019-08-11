b6c21e07246e8773530cb56816703983dfd5839e
Magdalena Trzeciak <Magda.Trzeciak@gmail.com>
2019-08-11 13:51:25 +0200

    Add files via upload
    initial commit

---
+++ lib/model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class SimpleFFDQN(nn.Module):
+    def __init__(self, obs_len, actions_n):
+        super(SimpleFFDQN, self).__init__()
+
+        self.fc_val = nn.Sequential(
+            nn.Linear(obs_len, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+        self.fc_adv = nn.Sequential(
+            nn.Linear(obs_len, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, actions_n)
+        )
+
+    def forward(self, x):
+        val = self.fc_val(x)
+        adv = self.fc_adv(x)
+        return val + adv - adv.mean(dim=1, keepdim=True)
